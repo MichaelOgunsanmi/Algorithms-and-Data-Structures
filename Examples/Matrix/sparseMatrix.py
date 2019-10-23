941b5b10a8c96bc8c305e3911e18adc6883fd054
Ogunsanmi <ogunsanmimichael@gmail.com>
2019-10-23 21:12:48 +0100

    Graph data structure implementation with Java

---
+++ Examples/Matrix/sparseMatrix.py
@@ -0,0 +1,105 @@
+def transformToSparseMatrix(normalMatrix):
+    rows = len(normalMatrix)
+    columns = len(normalMatrix[0])
+    output = []
+    output.append([rows,columns])
+
+    for row in range(rows):
+        for column in range(columns):
+            if normalMatrix[row][column] != 0:
+                output.append([row + 1,column + 1,normalMatrix[row][column]])
+
+    output[0].append(len(output) - 1)
+    return output
+
+
+# print(transformToSparseMatrix([
+# [1,0,0,0,0], 
+# [0,9,0,1,0], 
+# [0,0,0,0,0], 
+# [0,0,0,6,0]
+# ]))
+
+def addTwoSparseMatrices(firstSparseMatrix, secondSparseMatrix):
+    firstMatrix = transformToSparseMatrix(firstSparseMatrix)
+    secondMatrix = transformToSparseMatrix(secondSparseMatrix)
+    output = []
+
+    if firstMatrix[0][0] != secondMatrix[0][0] or firstMatrix[0][1] != secondMatrix[0][1]:
+        return
+
+    output.append([len(firstSparseMatrix), len(firstSparseMatrix[0])])
+    firstIterator = 1
+    secondIterator = 1
+    while firstIterator < len(firstMatrix) and secondIterator < len(secondMatrix):
+        if firstMatrix[firstIterator][0] > secondMatrix[secondIterator][0]:
+            output.append(secondMatrix[secondIterator])
+            secondIterator += 1
+        elif firstMatrix[firstIterator][0] < secondMatrix[secondIterator][0]:
+            output.append(firstMatrix[firstIterator])
+            firstIterator += 1
+        else:
+            if firstMatrix[firstIterator][1] > secondMatrix[secondIterator][1]:
+                output.append(secondMatrix[secondIterator])
+                secondIterator += 1
+            elif firstMatrix[firstIterator][1] < secondMatrix[secondIterator][1]:
+                output.append(firstMatrix[firstIterator])
+                firstIterator += 1
+            else:
+                output.append([firstMatrix[firstIterator][0], firstMatrix[firstIterator][1], firstMatrix[firstIterator][2] + secondMatrix[secondIterator][2]])
+                firstIterator += 1
+                secondIterator += 1
+    
+    if firstIterator < len(firstMatrix):
+        for i in range(firstIterator, len(firstMatrix)):
+            output.append(firstMatrix[i])
+    elif secondIterator < len(secondMatrix):
+        for i in range(secondIterator, len(secondMatrix)):
+            output.append(secondMatrix[i])
+
+    output[0].append(len(output) - 1)
+    
+    return output
+
+
+# print(addTwoSparseMatrices([
+# [0,0,3,0,0], 
+# [4,0,0,0,7], 
+# [0,0,5,0,8], 
+# [0,6,0,6,0]
+# ], [
+# [1,0,0,0,0], 
+# [0,9,0,1,0], 
+# [0,0,0,0,0], 
+# [0,0,0,6,0]
+# ]))
+
+# print(addTwoSparseMatrices([
+# [2,0,0,0,0], 
+# [2,0,0,0,0], 
+# [2,0,0,0,0], 
+# [2,0,0,0,0]
+# ], [
+# [1,1,1,1,1], 
+# [0,0,0,0,0], 
+# [0,0,0,0,0], 
+# [0,0,0,0,0]
+# ]))
+
+def transformToNormalMatrix(sparseMatrix):
+    rows = sparseMatrix[0][0]
+    columns = sparseMatrix[0][1]
+
+    output = [[0] * columns for i in range(rows)]
+
+    for i in range(1, sparseMatrix[0][2] + 1):
+        print(sparseMatrix[i])
+
+
+transformToNormalMatrix(transformToSparseMatrix([
+[1,0,0,0,0], 
+[0,9,0,1,0], 
+[0,0,0,0,0], 
+[0,0,0,6,0]
+]))
+
--- Java Implementations/src/com/dataStructuresGraph/Graph.java
+++ Java Implementations/src/com/dataStructuresGraph/Graph.java
@@ -1,9 +1,7 @@
 package com.dataStructuresGraph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.security.PublicKey;
+import java.util.*;
 
 public class Graph {
     private class Node{
@@ -46,5 +44,152 @@ public class Graph {
             if (!connectedNodesArray.isEmpty ())
                 System.out.println ( node + " is connected to " + connectedNodesArray);
         }
+    }
+
+    public void removeNode(String label){
+        var nodeToBeRemoved = graph.get ( label );
+        if (nodeToBeRemoved == null)
+            return;
+
+        for (Node node : adjacencyList.keySet ( ))
+            adjacencyList.get ( node ).remove ( nodeToBeRemoved ); //breaking connections between other nodes and nodeToBeRemoved;
+
+        adjacencyList.remove ( nodeToBeRemoved );
+        graph.remove ( nodeToBeRemoved );
+    }
+
+    public void removeEdge(String firstLabel, String secondLabel){
+        var firstNode = graph.get ( firstLabel );
+        var secondNode = graph.get ( secondLabel );
+
+        if (firstNode == null || secondNode == null)
+            return;
+
+        adjacencyList.get ( firstNode ).remove ( secondNode );
+    }
+
+    public void traverseDepthFirst(String startingLabel){
+        var startingNode = graph.get ( startingLabel );
+        if (startingNode == null)
+            return;
+
+        traverseDepthFirst ( startingNode, new HashSet<> () );
+    }
+
+    private void traverseDepthFirst ( Node currentNode, Set<Node> visited ) {
+        System.out.println ( currentNode );
+        visited.add(currentNode);
+
+        for (Node neighbour : adjacencyList.get ( currentNode ))
+            if (!visited.contains ( neighbour ))
+                traverseDepthFirst ( neighbour, visited );
+    }
+
+    public void traverseDepthFirstIter(String startingLabel){
+        var startingNode = graph.get ( startingLabel );
+        if (startingNode == null)
+            return;
+
+        Set<Node> visited = new HashSet<> ();
+        Stack<Node> stack = new Stack<> ();
+        stack.push ( startingNode );
+
+        while (!stack.isEmpty ()){
+            var currentNode = stack.pop ();
+            if (visited.contains ( currentNode ))
+                continue;
+
+            System.out.println (currentNode );
+            visited.add(currentNode);
+
+            for (Node neighbour : adjacencyList.get ( currentNode ))
+                if (!visited.contains ( neighbour ))
+                    stack.push ( neighbour );
+        }
+    }
+
+    public void traverseBreadthFirst(String startingLabel){
+        var startingNode = graph.get ( startingLabel );
+        if (startingNode == null)
+            return;
+
+        Set<Node> visited = new HashSet<> ();
+        Queue<Node> queue = new ArrayDeque<> ();
+        queue.add ( startingNode );
+
+        while (!queue.isEmpty ()){
+            var currentNode = queue.remove ();
+            if (visited.contains ( currentNode ))
+                continue;
+
+            System.out.println (currentNode );
+            visited.add(currentNode);
+
+            for (Node neighbour : adjacencyList.get ( currentNode ))
+                if (!visited.contains ( neighbour ))
+                    queue.add ( neighbour );
+        }
+    }
+
+    public List<String> topologicalSort(){
+        Stack<Node> stack = new Stack<> ();
+        Set<Node> visited = new HashSet<> ();
+
+        for (Node neighbour : graph.values ( ))
+            topologicalSort (neighbour, visited, stack);
+
+        List<String> sorted = new ArrayList<> ();
+        while (!stack.isEmpty ())
+            sorted.add ( stack.pop ().label );
+
+        return sorted;
+        }
+
+    private void topologicalSort ( Node currentNode, Set<Node> visited, Stack<Node> stack ) {
+        if (visited.contains ( currentNode ))
+            return;
+
+        visited.add ( currentNode );
+
+        for (Node neighbour : adjacencyList.get ( currentNode ))
+            topologicalSort ( neighbour, visited, stack );
+
+        stack.push ( currentNode );
+    }
+
+    public boolean hasCycle(){
+        Set<Node> all = new HashSet<> ();
+        all.addAll ( graph.values ());
+
+        Set<Node> visiting = new HashSet<> ();
+        Set<Node> visited = new HashSet<> ();
+
+        while (!all.isEmpty ()){
+            var currentNode = all.iterator ().next ();
+            if (hasCycle (currentNode, all, visiting, visited))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean hasCycle ( Node currentNode, Set<Node> all, Set<Node> visiting, Set<Node> visited ) {
+        all.remove ( currentNode );
+        visiting.add ( currentNode );
+
+        for (Node neighbour : adjacencyList.get ( currentNode )) {
+            if (visited.contains ( neighbour ))
+                continue;
+
+            if (visiting.contains ( neighbour ))
+                return true;
+
+            if (hasCycle ( neighbour, all, visiting, visited ))
+                return true;
+        }
+
+        visiting.remove ( currentNode );
+        visited.add ( currentNode );
+
+        return false;
     }
 }
