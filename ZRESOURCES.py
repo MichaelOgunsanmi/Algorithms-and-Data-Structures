b4b29728a373d78ce44b12d1ed46a6ff4c78398f
MichaelOgunsanmi <Ogunsanmimichael@gmail.com>
2020-06-29 06:57:52 +0100

    Updated ZRESOURCES

--- ZRESOURCES.py
+++ ZRESOURCES.py
@@ -1,1 +1,2 @@
 # https://leetcode.com/discuss/general-discussion/475924/My-experience-and-notes-for-learning-DP
+# https://leetcode.com/discuss/interview-question/481968/Amazon-Interview-Problems-List-(Updated)
